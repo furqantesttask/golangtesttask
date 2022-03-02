474c8aeff6021ff707cffb6d9ca926f0bb56ea2a
furqantesttask <furqan.dev20@gmail.com>
2022-03-02 17:59:48 +0500

    task # 1 - averageNumber function

--- task/task.go
+++ task/task.go
@@ -23,6 +23,26 @@ func TestValidity(str string) bool {
 	return true
 }
 
+func AverageNumber(str string) float64 {
+
+	var average float64
+	if TestValidity(str) {
+		splitted_array := strings.Split(str, "-")
+		var numbers_count, numbers_total float64
+
+		for index := 0; index < len(splitted_array); index++ {
+			if modChecker(index) {
+				intVar, _ := strconv.Atoi(splitted_array[index])
+
+				numbers_count++
+				numbers_total += float64(intVar)
+			}
+		}
+		average = (numbers_total / numbers_count)
+	}
+	return average
+}
+
 func modChecker(i int) bool {
 	if i%2 == 0 {
 		return true
